aac4c68734f6780e76531a7927c7a7dbed416398
Alexandre <Alexandre@PC-ALEX>
2014-05-22 00:22:43 +0200

    Demande console ou graphique

--- src/fr/iutvalence/tp1a/binome4/morpion/Appli.java
+++ src/fr/iutvalence/tp1a/binome4/morpion/Appli.java
@@ -1,33 +1,49 @@
 package fr.iutvalence.tp1a.binome4.morpion;
 
+import java.util.Scanner;
+
 import javax.swing.SwingUtilities;
 
 /**
  * Application.
- *
+ * 
  * @author Prinsac & Culty
  * @version 1.1
  */
 @SuppressWarnings("HardCodedStringLiteral")
 public final class Appli {
-    /** Application générale. */
-    public static void main(final String... args) {
-	SwingUtilities.invokeLater(new Fenetre());
-	/*final Scanner reader = new Scanner(System.in, "UTF-8");
-        System.out.println("Entrez le pseudo du joueur 1 (X) :");
-        final String pseudoJ1S = reader.nextLine();
-        final Joueur joueur1 = new Joueur(pseudoJ1S, Pion.JOUEUR1);
-        System.out.println("Entrez le pseudo du joueur 2 (O) :");
-        final String pseudoJ2S = reader.nextLine();
-        final Joueur joueur2 = new Joueur(pseudoJ2S, Pion.JOUEUR2);
-        final Score gestionnaireScore = new Score(joueur1, joueur2);
-        boolean rejouer = true;
-        while (rejouer) {
-            final Partie partie = new Partie(joueur1, joueur2, gestionnaireScore);
-            partie.jouer();
-            System.out.println("Voulez-vous rejouer ?");
-            System.out.println("1 pour rejouer, 0 pour arrêter.");
-            rejouer = reader.nextInt() == 1;
-        }*/
-    }
+	/** Application générale. */
+	public static void main(final String... args) {
+		final Scanner reader = new Scanner(System.in, "UTF-8");
+		boolean choixInterface = false;
+		String consoleOuGraph = "";
+		while (!choixInterface) {
+			System.out.println("Pour jouer en console tapez 'c', pour jouer en graphique tapez 'g'.");
+			consoleOuGraph = reader.nextLine();
+			if((consoleOuGraph.equalsIgnoreCase("c")) || (consoleOuGraph.equalsIgnoreCase("g"))){
+				choixInterface = true;
+			}
+		}
+		if (consoleOuGraph.equalsIgnoreCase("c")) {
+			System.out.println("Entrez le pseudo du joueur 1 (X) :");
+			final String pseudoJ1S = reader.nextLine();
+			System.out.println("Entrez le pseudo du joueur 2 (O) :");
+			final String pseudoJ2S = reader.nextLine();
+			final Joueur joueur1 = new Joueur(pseudoJ1S, Pion.JOUEUR1);
+			final Joueur joueur2 = new Joueur(pseudoJ2S, Pion.JOUEUR2);
+			final Score gestionnaireScore = new Score(joueur1, joueur2);
+			boolean rejouer = true;
+			while (rejouer) {
+				final Partie partie = new Partie(joueur1, joueur2,
+						gestionnaireScore);
+				partie.jouer();
+				System.out.println("Voulez-vous rejouer ?");
+				System.out.println("1 pour rejouer, 0 pour arrêter.");
+				rejouer = reader.nextInt() == 1;
+			}
+		} else if (consoleOuGraph.equalsIgnoreCase("g")) {
+			SwingUtilities.invokeLater(new Fenetre());
+		}
+	}
+
 }
--- src/fr/iutvalence/tp1a/binome4/morpion/Fenetre.java
+++ src/fr/iutvalence/tp1a/binome4/morpion/Fenetre.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,102 +16,126 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 
 public class Fenetre implements Runnable {
-    private PartieIHM partieIHM;
-    private JTextField jtf1;
-    private JTextField jtf2;
+	private PartieIHM partieIHM;
+	private JTextField jtf1;
+	private JTextField jtf2;
+	private JLabel lab1;
+	private JLabel lab2;
 
-    private JButton buton;
+	private JButton buton;
 
-    private JFrame demande;
-    private JFrame tabMorpion;
+	private JFrame demande;
+	private JFrame tabMorpion;
 
-    @Override
-    public void run() {
-	demanderJoueurs();
-	jeu();
-    }
-
-    private void demanderJoueurs() {
-	demande = new JFrame("Morpion");
-	demande.setSize(600, 600);
-	// demande.setDefaultCloseOperation(JFrame.D);
-	demande.setLocationRelativeTo(null);
-	JPanel container = new JPanel();
-	container.setBackground(Color.white);
-	container.setLayout(new BorderLayout());
-	JPanel top = new JPanel();
-	jtf1 = new JTextField("Joueur1");
-	jtf1.setPreferredSize(new Dimension(100, 25));
-	jtf1.setForeground(Color.BLUE);
-	JLabel label1 = new JLabel("Pseudo joueur 1");
-	top.add(label1);
-	top.add(jtf1);
-	jtf2 = new JTextField("Joueur2");
-	jtf2.setPreferredSize(new Dimension(100, 25));
-	jtf2.setForeground(Color.BLUE);
-	JButton valide = new JButton("Valider");
-	valide.addActionListener(new BoutonListener());
-	top.add(new JLabel("Pseudo joueur 2"));
-	top.add(jtf2);
-	top.add(valide);
-	container.add(top, BorderLayout.NORTH);
-	demande.setContentPane(container);
-	demande.setVisible(true);
-	demande.pack();
-    }
-
-    private void jeu() {
-	tabMorpion = new JFrame("Morpion");
-	tabMorpion.setSize(600, 600);
-	// demande.setDefaultCloseOperation(JFrame.D);
-	tabMorpion.setLocationRelativeTo(null);
-	JPanel container = new JPanel();
-	container.setBackground(Color.white);
-	JPanel top = new JPanel();
-
-	tabMorpion.setLayout(new GridLayout(3, 3));
-
-	for (int numeroDeLigne = 1; numeroDeLigne < 4; numeroDeLigne++) {
-	    for (int numeroDeColonne = 1; numeroDeColonne < 4; numeroDeColonne++) {
-		JButton bouton = new Bouton(numeroDeLigne, numeroDeColonne);
-		tabMorpion.add(bouton);
-	    }
+	@Override
+	public void run() {
+		demanderJoueurs();
+		jeu();
 	}
 
-	tabMorpion.setVisible(false);
-
-    }
-
-    private class Bouton extends JButton {
-	private int ligne;
-	private int colonne;
-	private Pion pion;
-
-	Bouton(int numLigne, int numColonne) {
-	    ligne = numLigne;
-	    colonne = numColonne;
-	    addActionListener(new BListener());
+	private void demanderJoueurs() {
+		/** Fenetre globale */
+		demande = new JFrame("Morpion");
+		demande.setSize(500, 500);
+		demande.setResizable(false);
+		demande.setLocationRelativeTo(null);
+		demande.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		/** Conteneur global */
+		JPanel container = new JPanel();
+		container.setBackground(Color.white);
+		
+		/** Police */
+		Font police = new Font("Arial", Font.BOLD, 16);
+		
+		/** Champ 1 */
+		jtf1 = new JTextField("Joueur1");
+		jtf1.setPreferredSize(new Dimension(230, 40));
+	    jtf1.setFont(police);
+		jtf1.setForeground(Color.BLUE);
+		lab1 = new JLabel("Pseudo joueur 1");
+		lab1.setFont(police);
+		container.add(lab1);
+		container.add(jtf1);
+		
+		/** Champ 2 */
+		jtf2 = new JTextField("Joueur2");
+		jtf2.setPreferredSize(new Dimension(230, 40));
+	    jtf2.setFont(police);
+		jtf2.setForeground(Color.BLUE);
+		lab2 = new JLabel("Pseudo joueur 2");
+		lab2.setFont(police);
+		container.add(lab2);
+		container.add(jtf2);
+		
+		/** Bouton de validation */
+		JButton valide = new JButton("Valider");
+		valide.setFont(police);
+		valide.addActionListener(new BoutonListener());
+		container.add(valide);
+		
+		/** Traitement divers */
+		demande.setContentPane(container);
+		demande.setVisible(true);
 	}
 
-	class BListener implements ActionListener {
-	    public void actionPerformed(ActionEvent e) {
-		if (partieIHM.estLibre(ligne, colonne)){
-		    partieIHM.poserPion(ligne,colonne,m_joueurs[m_tour].pion());
-		    
+	private void jeu() {
+		/** Fenetre globale */
+		tabMorpion = new JFrame("Morpion");
+		tabMorpion.setSize(500, 500);
+		tabMorpion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tabMorpion.setLocationRelativeTo(null);
+		
+		/** Conteneur global */
+		JPanel container = new JPanel();
+		container.setBackground(Color.white);
+		
+		/** Conteneur haut */
+		JPanel top = new JPanel();
+
+		/** Grille de jeu */
+		tabMorpion.setLayout(new GridLayout(3, 3));
+
+		for (int numeroDeLigne = 1; numeroDeLigne < 4; numeroDeLigne++) {
+			for (int numeroDeColonne = 1; numeroDeColonne < 4; numeroDeColonne++) {
+				JButton bouton = new Bouton(numeroDeLigne, numeroDeColonne);
+				bouton.addActionListener(new BListener());
+				tabMorpion.add(bouton);
+			}
 		}
-	    }
-	}
-    }
 
-    class BoutonListener implements ActionListener {
-	public void actionPerformed(ActionEvent e) {
-	    final Joueur joueur1 = new Joueur(jtf1.getText(), Pion.JOUEUR1);
-	    final Joueur joueur2 = new Joueur(jtf2.getText(), Pion.JOUEUR2);
-	    final Score gestionnaireScore = new Score(joueur1, joueur2);
-	    partieIHM = new PartieIHM(joueur1, joueur2, gestionnaireScore);
-	    demande.setVisible(false);
-	    tabMorpion.setVisible(true);
-
+		/** Traitement divers */
+		tabMorpion.setVisible(false);
 	}
-    }
+
+	private class Bouton extends JButton {
+		private int ligne;
+		private int colonne;
+		private Pion pion;
+
+		Bouton(int numLigne, int numColonne) {
+			ligne = numLigne;
+			colonne = numColonne;
+			addActionListener(new BListener());
+		}
+
+		class BListener implements ActionListener {
+			public void actionPerformed(ActionEvent e) {
+				if (partieIHM.estLibre(ligne, colonne)) {
+					partieIHM.poserPion(ligne, colonne, m_joueurs[m_tour].pion());
+				}
+			}
+		}
+	}
+
+	class BoutonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			final Joueur joueur1 = new Joueur(jtf1.getText(), Pion.JOUEUR1);
+			final Joueur joueur2 = new Joueur(jtf2.getText(), Pion.JOUEUR2);
+			final Score gestionnaireScore = new Score(joueur1, joueur2);
+			partieIHM = new PartieIHM(joueur1, joueur2, gestionnaireScore);
+			demande.setVisible(false);
+			tabMorpion.setVisible(true);
+		}
+	}
 }
